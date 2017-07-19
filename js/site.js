4a5eb5d4f36241b22dfb889c2c3957f5ab2be9f0
Matmonsen <sindre.nerdal@gmail.com>
2017-07-19 22:54:17 +0200

    Functionality is finished. Needs ui makeover

--- js/site.js
+++ js/site.js
@@ -1,43 +1,76 @@
 $(document).ready(function() {
-    var count = 0;
-    var template = '<div class="friend-row" friend-id="">' +
-        '<div class="col-xs-6 col-sm-4">' +
-        '<button type="button" class="btn btn-danger btn-block remove-friend" id="">Remove</button>' +
-        '</div>' +
-
-        '<div class=" col-xs-6 col-sm-8">' +
-        '<div class="input-group">' +
-        '<input type="text" class="form-control date" placeholder="Friend"/>' +
-        '<span class="input-group-addon">' +
-        '<span class="glyphicon glyphicon-calendar"></span>' +
-        '</span>' +
-        '</div>' +
-        '</div>' +
-        '</div>';
+    var rowIdAttr = "friend-id";
+    var dateFields = ".date";
+    var dateFormat = "DD/MM/YYYY";
+    var friendIdCounter = 0;
+    var template = $("#template").html();
 
 
-    var calculate = function() {
+    var sortByMonthThenDay = function(d1, d2) {
+        if (d1.month() > d2.month())
+            return true;
 
+        if (d1.month() === d2.month())
+            return d1.date() > d2.date();
+
+        return false;
     };
 
+    var midDateBetween = function(d1, d2) {
+        return moment((d1 + d2) / 2);
+    };
 
+    var makeListOfDatesEven = function(allDates) {
+        if (allDates.length % 2 !== 0)
+            allDates.push(allDates[allDates.length - 1]);
+        return allDates;
+    };
 
+    var setAnswer = function(friendDate) {
+        if (typeof friendDate !== "undefined")
+            $("#answer").text("Friend-day is " + friendDate.format(dateFormat) + "!")
+    };
+
+    var getDatesFromInputFields = function() {
+        var allDates = [];
+        $(dateFields).each(function() {
+            var d = moment($(this).val(), dateFormat);
+            if (!isNaN(d))
+                allDates.push(d);
+
+        });
+        return allDates;
+    };
+
+    var findFriendDate = function(allDates) {
+
+        while (allDates.length  > 1) {
+            var next = [];
+            for(var i = 0; i < allDates.length; i+=2) {
+                var midDate = midDateBetween(allDates[i], allDates[i + 1]);
+                next.push(midDate)
+            }
+            allDates = [];
+            allDates = next;
+        }
+        return allDates[0];
+    };
 
     var addFriend = function() {
         var newRow = $(template);
-        newRow.find("button").attr("id", count);
-        newRow.attr("friend-id", count);
-        count++;
+        newRow.find("button").attr("id", friendIdCounter);
+        newRow.attr(rowIdAttr, friendIdCounter);
+        friendIdCounter++;
         newRow.appendTo('.rows');
-        $('.date').datetimepicker({
-            format: 'DD/MM/YYYY'
+        $(dateFields).datetimepicker({
+            format: dateFormat
         });
     };
     var removeFriend = function (elm) {
         var id = $(elm).attr("id");
 
         $('.rows').children('div').each(function(index, element) {
-            if($(element).attr("friend-id") === id) {
+            if($(element).attr(rowIdAttr) === id) {
                 $(element).remove();
             }
         });
@@ -47,14 +80,23 @@ $(document).ready(function() {
         $('#calculate-button').attr("disabled", $('.rows').children('div').length - 1 === 0);
     };
 
+    // Listeners
     $('#add-friend-button').click(function() {
         addFriend();
         disableCalculate();
     });
-
     $(document).on('click', '.remove-friend',  function() {
         removeFriend(this);
         disableCalculate();
+    });
+    $("#calculate-button").click(function() {
+        var allDates = getDatesFromInputFields();
+        allDates.sort(sortByMonthThenDay);
+        allDates = makeListOfDatesEven(allDates);
+
+        var friendDate = findFriendDate(allDates);
+        setAnswer(friendDate);
+
     });
 
     addFriend();
